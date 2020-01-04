55aa4b591894b8325dad90b96c631666e4016e8f
fwztheone <fabrizio.villani@std.heh.be>
2020-01-04 22:23:53 +0100

    ajout des valvesé

--- app/src/main/java/campus/technique/heh/projetautomate/AutomateRegulation.java
+++ app/src/main/java/campus/technique/heh/projetautomate/AutomateRegulation.java
@@ -18,8 +18,16 @@ public class AutomateRegulation extends AppCompatActivity {
 
         private  TextView tv_liquide;
         private  TextView tv_ref;
-        private  TextView tv_etat;
-        private  TextView tv_consigneA;
+        //ensemble des valves
+        private  TextView tv_etat1;
+        private  TextView tv_etat2;
+        private  TextView tv_etat3;
+        private  TextView tv_etat4;
+        private  TextView tv_etat5;
+        private  TextView tv_etat6;
+
+
+    private  TextView tv_consigneA;
         private  TextView tv_consigneM;
         private  TextView tv_motpilotage;
 
@@ -36,7 +44,14 @@ public class AutomateRegulation extends AppCompatActivity {
 
         tv_liquide = (TextView)findViewById(R.id.tv_liquide_plc);
         tv_ref = (TextView)findViewById(R.id.tv_main_plc);
-        tv_etat = (TextView)findViewById(R.id.tv_etat_plc);
+        //liste des valves
+        tv_etat1 = (TextView)findViewById(R.id.tv_etat1_plc);
+        tv_etat2 = (TextView)findViewById(R.id.tv_etat2_plc);
+        tv_etat3 = (TextView)findViewById(R.id.tv_etat3_plc);
+        tv_etat4 = (TextView)findViewById(R.id.tv_etat4_plc);
+        tv_etat5 = (TextView)findViewById(R.id.tv_etat5_plc);
+        tv_etat6 = (TextView)findViewById(R.id.tv_etat6_plc);
+
         tv_consigneA = (TextView)findViewById(R.id.tv_consigne_plc);
         tv_consigneM = (TextView)findViewById(R.id.tv_consigneM_plc);
         tv_motpilotage = (TextView)findViewById(R.id.tv_pilotage_plc);
@@ -57,7 +72,7 @@ public class AutomateRegulation extends AppCompatActivity {
             }
 //                public ReadTS7(TextView liquide, TextView status, TextView setpoint, TextView consigne, TextView pilotage, TextView ref){
 
-                rest1 = new ReadTS7(tv_liquide,tv_etat ,tv_consigneA,tv_consigneM,tv_motpilotage,tv_ref);
+                rest1 = new ReadTS7(tv_liquide,tv_etat1 ,tv_consigneA,tv_consigneM,tv_motpilotage,tv_ref,tv_etat2,tv_etat3,tv_etat4,tv_etat5,tv_etat6);
             rest1.Start(ip,rack,slot);
         }
 
--- app/src/main/java/campus/technique/heh/projetautomate/ReadTS7.java
+++ app/src/main/java/campus/technique/heh/projetautomate/ReadTS7.java
@@ -26,6 +26,15 @@ public class ReadTS7 {
 
 
 
+    //les valves
+    private TextView valve1;
+    private TextView valve2;
+    private TextView valve3;
+    private TextView valve4;
+    private TextView valve5;
+    private TextView valve6;
+
+
 
 
 
@@ -36,7 +45,9 @@ public class ReadTS7 {
 
     //TextView pour le liquide
     private TextView tv_liquide_plc;
-    protected TextView tv_status_plc;
+    private TextView tv_status_plc;
+    private TextView tv_valve1_plc;
+
 
     private TextView tv_setpoint_plc;
     private TextView tv_consigneM_plc;
@@ -60,17 +71,25 @@ public class ReadTS7 {
     private byte[] motpilotage = new byte[512];
     private byte[] statusPLC = new byte[512];
 
+    private byte[] statusPLC2 = new byte[512];
 
 
 
-    public ReadTS7(TextView liquide, TextView status, TextView setpoint, TextView consigne, TextView pilotage, TextView ref){
+
+    public ReadTS7(TextView liquide, TextView valve1, TextView setpoint, TextView consigne, TextView pilotage, TextView ref, TextView valve2, TextView vavle3,TextView vavle4,TextView vavle5,TextView vavle6){
 
         tv_liquide_plc = liquide;
-        tv_status_plc = status;
+        tv_valve1_plc = valve1;
         tv_setpoint_plc =setpoint;
         tv_consigneM_plc =consigne;
         tv_mot_pilotage =pilotage;
         tv_main_plc = ref;
+
+        this.valve2 = valve2;
+        this.valve3 = vavle3;
+        this.valve4 = vavle4;
+        this.valve5 = vavle5;
+        this.valve6 = vavle6;
 
         comS7 = new S7Client();
         plcS7 = new AutomateS7();
@@ -106,16 +125,64 @@ public class ReadTS7 {
         tv_mot_pilotage.setText(("Mot de pilotage vanne : 0"));
         tv_consigneM_plc.setText("Consigne manuelle : 0");
         tv_setpoint_plc.setText("Consigne automatique : 0 ");
-        tv_status_plc.setText("Etat de fonctionnement : 0");
+        tv_valve1_plc.setText("Etat de fonctionnement : désactivé");
+        valve2.setText("Etat de fonctionnement : désactivé");
+        valve3.setText("Etat de fonctionnement : désactivé");
+        valve4.setText("Etat de fonctionnement : désactivé");
+        valve5.setText("Etat de fonctionnement : désactivé");
+        valve6.setText("Etat de fonctionnement : désactivé");
+
+
 
 
     }
 
-    private void downloadOnPreExecute(int liquide, int option, int consM, int etat , int ref , int pilot ){
+    private void downloadOnPreExecute(int liquide, int option, int consM, int data1 , int ref , int pilot,int data2, int data3, int data4, int data5, int data6 ){
         tv_liquide_plc.setText("LIQUIDE : " +  String.valueOf(liquide));
         tv_setpoint_plc.setText("CONSIGNE AUTO : " +  String.valueOf(option));
         tv_consigneM_plc.setText("Consigne manuelle : " + String.valueOf(consM));
-        tv_status_plc.setText("Etat de fonctionnement : " + String.valueOf(etat));
+        if(data1 == 0){
+            tv_valve1_plc.setText("état Valve 1 : désactivé" );
+        }
+        else{
+            tv_valve1_plc.setText("état Valve 1 : activé" );
+
+        }
+        if(data2 == 0){
+            valve2.setText("état Valve 2 : désactivé" );
+        }
+        else{
+            valve2.setText("état Valve 2 : activé" );
+
+        }
+        if(data3 == 0){
+            valve3.setText("état Valve 3 : désactivé" );
+        }
+        else{
+            valve3.setText("état Valve 3 : activé" );
+
+        }
+        if(data4 == 0){
+            valve4.setText("état Valve 4 : désactivé" );
+        }
+        else{
+            valve4.setText("état Valve 4 : activé" );
+
+        }
+        if(data5 == 0){
+            valve5.setText("Bouton automatique : désactivé" );
+        }
+        else{
+            valve5.setText("Bouton automatique  : activé" );
+
+        }
+        if(data6 == 0){
+            valve6.setText("Local Distant : désactivé" );
+        }
+        else{
+            valve6.setText("Local Distant : activé" );
+
+        }
         tv_main_plc.setText(" information de l'automate modèle : " + String.valueOf(ref));
         tv_mot_pilotage.setText("mot de pilotage vanne : " + String.valueOf(pilot));
 
@@ -145,12 +212,17 @@ public class ReadTS7 {
 //                    bundle.putInt("reference", refe);
 //                    bundle.putInt("pilotageAuto", pilot);
 
-                    int etat = data.getInt("etat",0);
+                    int data1 = data.getInt("etat",0);
                     int reference = data.getInt("reference",0);
                     int pilotageAuto = data.getInt("pilotageAuto",0);
+                    int datav2 = data.getInt("datav2",0);
+                    int datav3 = data.getInt("datav3",0);
+                    int datav4 = data.getInt("datav4",0);
+                    int datav5 = data.getInt("datav5",0);
+                    int datav6 = data.getInt("datav6",0);
 
 
-                    downloadOnPreExecute(msg.arg1, msg.arg2, consigneM,etat,reference,pilotageAuto);
+                    downloadOnPreExecute(msg.arg1, msg.arg2, consigneM,data1,reference,pilotageAuto,datav2,datav3,datav4,datav5,datav6);
 
 
                     break;
@@ -206,6 +278,7 @@ public class ReadTS7 {
                         int pilotage = comS7.ReadArea(S7.S7AreaDB,5,22,8,motpilotage);
 
                         int etat = comS7.ReadArea(S7.S7AreaDB,5,0,2,statusPLC);
+                        int etat2 = comS7.ReadArea(S7.S7AreaDB,5,1,2,statusPLC2);
 
                         int data=0;
                         int dataB=0;
@@ -214,16 +287,34 @@ public class ReadTS7 {
                         int dataE = 0;
                         int dataF =0;
 
+
+                        //valve;
+                        int dataV2 = 0;
+                        int dataV3 = 0;
+                        int dataV4 = 0;
+                        int dataV5 = 0;
+                        int dataV6 = 0;
+
                         if (retInfo==0) {
                             data = S7.GetWordAt(datasPLC,0);
                             dataB = S7.GetWordAt(volumePLC,0);
                             datac = S7.GetWordAt(consigneAuto,0);
                             dataD = S7.GetWordAt(consigneManu,0);
-                            dataE = S7.GetWordAt(statusPLC,0);
                             dataF = S7.GetWordAt(motpilotage,0);
 
 
-                            sendPreExecuteMessage(dataB,datac,dataD,dataE,numCPU,dataF);
+
+
+                            dataV2 = S7.GetBitAt(statusPLC,0,2) ? 1 : 0;
+                            dataV3 = S7.GetBitAt(statusPLC,0,3) ? 1 : 0;
+                            dataV4 = S7.GetBitAt(statusPLC,0,4) ? 1 : 0;
+                            dataV5 = S7.GetBitAt(statusPLC,0,5) ? 1 : 0;
+                            dataV6 = S7.GetBitAt(statusPLC,0,6) ? 1 : 0;
+                            dataE = S7.GetBitAt(statusPLC,0,1) ? 1 : 0;
+
+
+
+                            sendPreExecuteMessage(dataB,datac,dataD,dataE,numCPU,dataF,dataV2,dataV3,dataV4,dataV5,dataV6);
 
 
 
@@ -260,7 +351,7 @@ public class ReadTS7 {
         }
 
 
-        private void sendPreExecuteMessage(int liquide, int consigneAuto, int consigneM, int status, int ref, int pilotage) {
+        private void sendPreExecuteMessage(int liquide, int consigneAuto, int consigneM, int status, int ref, int pilotage, int data2, int data3, int data4, int data5, int data6) {
             Message preExecuteMsg= new Message();
             preExecuteMsg.what= MESSAGE_PRE_EXECUTE;
             preExecuteMsg.arg1 = liquide;
@@ -272,12 +363,23 @@ public class ReadTS7 {
             int etat = status;
             int refe = ref;
             int pilot = pilotage;
+            int datav2 = data2;
+            int datav3= data3;
+            int datav4 = data4;
+            int datav5 = data5;
+            int datav6 = data6;
 
             Bundle bundle = new Bundle();
             bundle.putInt("consigneM", consigneManuelle);
             bundle.putInt("etat", etat);
             bundle.putInt("reference", refe);
             bundle.putInt("pilotageAuto", pilot);
+            bundle.putInt("datav2",datav2);
+            bundle.putInt("datav3",datav3);
+            bundle.putInt("datav4",datav4);
+            bundle.putInt("datav5",datav5);
+            bundle.putInt("datav6",datav6);
+
 
             preExecuteMsg.setData(bundle);
 
