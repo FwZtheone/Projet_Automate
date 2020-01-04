236cdc7ae8eb004ff0183425c617ddbb2fcae76d
fwztheone <fabrizio.villani@std.heh.be>
2020-01-04 20:49:53 +0100

    affichage des informations de l'automate manque plus que l'état

--- app/src/main/java/campus/technique/heh/projetautomate/ReadTS7.java
+++ app/src/main/java/campus/technique/heh/projetautomate/ReadTS7.java
@@ -1,6 +1,7 @@
 package campus.technique.heh.projetautomate;
 
 import android.annotation.SuppressLint;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
@@ -35,7 +36,8 @@ public class ReadTS7 {
 
     //TextView pour le liquide
     private TextView tv_liquide_plc;
-    private TextView tv_status_plc;
+    protected TextView tv_status_plc;
+
     private TextView tv_setpoint_plc;
     private TextView tv_consigneM_plc;
     private TextView tv_mot_pilotage;
@@ -96,52 +98,61 @@ public class ReadTS7 {
         }
     }
 
-//    //après affichage
-//    private void downloadOnPreExecute(int liquide, int ref,int pilotage, int consigne, int setpoint, int status){
-//        tv_liquide_plc.setText("LIQUIDE : " + String.valueOf(liquide) );
-//        tv_main_plc.append(String.valueOf(ref));
-//        tv_mot_pilotage.setText("Mot de pilotage vanne : " +String.valueOf(pilotage));
-//        tv_consigneM_plc.setText("Consigne manuelle :" + String.valueOf(consigne));
-//        tv_setpoint_plc.setText("Consigne automatique : " +String.valueOf(setpoint));
-//        tv_status_plc.setText("Etat de fonctionnement :" + String.valueOf(status));
-//
-//
-//    }
-//
-//    //avant affichage
-//    private void downloadOnPostExecute()
-//    {
-//        tv_liquide_plc.setText("LIQUIDE : 0");
-//        tv_main_plc.setText("Information automate : ");
-//        tv_mot_pilotage.setText(("Mot de pilotage vanne : "));
-//        tv_consigneM_plc.setText("Consigne manuelle :");
-//        tv_setpoint_plc.setText("Consigne automatique : ");
-//        tv_status_plc.setText("Etat de fonctionnement :");
-//
-//
-//    }
 
-    private void downloadOnPreExecute(int liquide){
-        tv_liquide_plc.setText("LIQUIDE : " +  String.valueOf(liquide));
-
-    }
-
-    private void downloadOnPostExecute(){
+    private void downloadOnPostExecute()
+    {
         tv_liquide_plc.setText("LIQUIDE : 0");
+        tv_main_plc.setText("Information automate : 0");
+        tv_mot_pilotage.setText(("Mot de pilotage vanne : 0"));
+        tv_consigneM_plc.setText("Consigne manuelle : 0");
+        tv_setpoint_plc.setText("Consigne automatique : 0 ");
+        tv_status_plc.setText("Etat de fonctionnement : 0");
+
+
     }
+
+    private void downloadOnPreExecute(int liquide, int option, int consM, int etat , int ref , int pilot ){
+        tv_liquide_plc.setText("LIQUIDE : " +  String.valueOf(liquide));
+        tv_setpoint_plc.setText("CONSIGNE AUTO : " +  String.valueOf(option));
+        tv_consigneM_plc.setText("Consigne manuelle : " + String.valueOf(consM));
+        tv_status_plc.setText("Etat de fonctionnement : " + String.valueOf(etat));
+        tv_main_plc.setText(" information de l'automate modèle : " + String.valueOf(ref));
+        tv_mot_pilotage.setText("mot de pilotage vanne : " + String.valueOf(pilot));
+
+    }
+
+
+
 
 
 
 
     @SuppressLint("HandlerLeak")
-    private Handler monHandler = new Handler() {
+    private final Handler monHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
+
             switch (msg.what) {
                 case MESSAGE_PRE_EXECUTE:
-                    Message msg2 = msg;
-                    downloadOnPreExecute(msg.arg1);
+
+                    //je recup mon tableau de data
+                    Bundle data = msg.getData();
+                    int consigneM = data.getInt("consigneM",0);
+
+//                    bundle.putInt("consigneM", consigneManuelle);
+//                    bundle.putInt("etat", etat);
+//                    bundle.putInt("reference", refe);
+//                    bundle.putInt("pilotageAuto", pilot);
+
+                    int etat = data.getInt("etat",0);
+                    int reference = data.getInt("reference",0);
+                    int pilotageAuto = data.getInt("pilotageAuto",0);
+
+
+                    downloadOnPreExecute(msg.arg1, msg.arg2, consigneM,etat,reference,pilotageAuto);
+
+
                     break;
 
                 case MESSAGE_POST_EXECUTE:
@@ -167,8 +178,8 @@ public class ReadTS7 {
 
                 S7OrderCode orderCode= new S7OrderCode();
                 Integer result= comS7.GetOrderCode(orderCode);
-                int numCPU;
-                numCPU = -1;
+                int numCPU = -1;
+
                 if (res.equals(0) && result.equals(0)){
                     //Quelques exemples : // WinAC: 6ES7 611-4SB00-0YB7
                     // S7-315 2DPP?N : 6ES7 315-4EH13-0AB0
@@ -179,6 +190,7 @@ public class ReadTS7 {
                 else
                 {
                     numCPU= 0000;
+
                 }
 
 
@@ -187,11 +199,11 @@ public class ReadTS7 {
                         //ref
                         int retInfo= comS7.ReadArea(S7.S7AreaDB,5,9,2,datasPLC);
                         //niveau de liquide
-                        int liquideInfo = comS7.ReadArea(S7.S7AreaDB,5,16,2,volumePLC);
+                        int liquideInfo = comS7.ReadArea(S7.S7AreaDB,5,16,8,volumePLC);
 
-                        int consigneA = comS7.ReadArea(S7.S7AreaDB,5,18,2,consigneAuto);
-                        int consigneM = comS7.ReadArea(S7.S7AreaDB,5,20,2,consigneManu);
-                        int pilotage = comS7.ReadArea(S7.S7AreaDB,5,22,2,motpilotage);
+                        int consigneA = comS7.ReadArea(S7.S7AreaDB,5,18,8,consigneAuto);
+                        int consigneM = comS7.ReadArea(S7.S7AreaDB,5,20,8,consigneManu);
+                        int pilotage = comS7.ReadArea(S7.S7AreaDB,5,22,8,motpilotage);
 
                         int etat = comS7.ReadArea(S7.S7AreaDB,5,0,2,statusPLC);
 
@@ -210,10 +222,18 @@ public class ReadTS7 {
                             dataE = S7.GetWordAt(statusPLC,0);
                             dataF = S7.GetWordAt(motpilotage,0);
 
+
+                            sendPreExecuteMessage(dataB,datac,dataD,dataE,numCPU,dataF);
+
+
+
+
 //                            private void sendPreExecuteMessage(int liquide, int ref, int pilotage, int consigneM, int consigneA, int etat ) {
-                            sendPreExecuteMessage(dataB,data,dataF,dataD,datac,dataE);
+
 
                         }
+                        Log.i("Variable A.P.I. -> ", String.valueOf(retInfo));
+
                         Log.i("Variable A.P.I. -> ", String.valueOf(data));
                         Log.i("Contenu du volume  -> ", String.valueOf(dataB));
                     }
@@ -240,25 +260,36 @@ public class ReadTS7 {
         }
 
 
-        private void sendPreExecuteMessage(int liquide, int ref, int pilotage, int consigneM, int consigneA, int etat ) {
+        private void sendPreExecuteMessage(int liquide, int consigneAuto, int consigneM, int status, int ref, int pilotage) {
             Message preExecuteMsg= new Message();
             preExecuteMsg.what= MESSAGE_PRE_EXECUTE;
             preExecuteMsg.arg1 = liquide;
-//            preExecuteMsg.arg2 = ref;
-//            preExecuteMsg.obj = pilotage;
-//            preExecuteMsg.obj = consigneM;
-//            preExecuteMsg.obj = consigneA;
-//            preExecuteMsg.obj = etat;
+            preExecuteMsg.arg2 = consigneAuto;
 
 
-//            tv_liquide_plc.setText("LIQUIDE : 0");
-//            tv_main_plc.setText("Information automate : ");
-//            tv_mot_pilotage.setText(("Mot de pilotage vanne : "));
-//            tv_consigneM_plc.setText("Consigne manuelle :");
-//            tv_setpoint_plc.setText("Consigne automatique : ");
-//            tv_status_plc.setText("Etat de fonctionnement :");
+
+            int consigneManuelle = consigneM;
+            int etat = status;
+            int refe = ref;
+            int pilot = pilotage;
+
+            Bundle bundle = new Bundle();
+            bundle.putInt("consigneM", consigneManuelle);
+            bundle.putInt("etat", etat);
+            bundle.putInt("reference", refe);
+            bundle.putInt("pilotageAuto", pilot);
+
+            preExecuteMsg.setData(bundle);
+
+
             monHandler.sendMessage(preExecuteMsg);
         }
+
+
+
+
+
+
 
 
 
